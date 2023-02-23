f57cd7a46219e2b5632933bbb30d925acccca077
Luca Herrero Marcos <luca@herreromarcos.com>
2023-02-23 13:49:33 +0100

    Update WebSocketConfig.java

--- src/main/java/com/placeteam/backend/server/WebSocketConfig.java
+++ src/main/java/com/placeteam/backend/server/WebSocketConfig.java
@@ -1,9 +1,12 @@
 package com.placeteam.backend.server;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketTransportRegistration;
+import org.springframework.web.socket.server.standard.ServletServerContainerFactoryBean;
 
 @Configuration
 @EnableWebSocket
@@ -17,5 +20,12 @@ public class WebSocketConfig implements WebSocketConfigurer {
 		registry.addHandler(socketHandler, WEBSOCKET_HANDLER_PATH);
 		
 		//SocketHandler.add(socketIncommingMessageHandler);
+	}
+
+	@Bean
+	public ServletServerContainerFactoryBean createWebSocketContainer() {
+		ServletServerContainerFactoryBean container = new ServletServerContainerFactoryBean();
+		container.setMaxTextMessageBufferSize(1024 * 1024);
+		return container;
 	}
 }
