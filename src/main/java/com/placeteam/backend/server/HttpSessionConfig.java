e4a4a244559dd42fef83ac5826564be0a9b5e651
Lukas Wurzinger <lukas@wrz.one>
2023-03-02 10:57:55 +0100

    HTTP Sessions

---
+++ src/main/java/com/placeteam/backend/server/HttpSessionConfig.java
@@ -0,0 +1,39 @@
+package com.placeteam.backend.server;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import jakarta.servlet.http.HttpSession;
+import jakarta.servlet.http.HttpSessionEvent;
+import jakarta.servlet.http.HttpSessionListener;
+
+import org.springframework.boot.web.servlet.ServletListenerRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class HttpSessionConfig {
+    private static final Map<String, HttpSession> sessions = new HashMap<>();
+
+    public static List<HttpSession> getActiveSessions() {
+        return new ArrayList<>(sessions.values());
+    }
+
+    @Bean
+    public ServletListenerRegistrationBean<HttpSessionListener> httpSessionListener() {
+        ServletListenerRegistrationBean<HttpSessionListener> listenerRegistrationBean = new ServletListenerRegistrationBean<>();
+        listenerRegistrationBean.setListener(new HttpSessionListener() {
+            @Override
+            public void sessionCreated(HttpSessionEvent hse) {
+                sessions.put(hse.getSession().getId(), hse.getSession());
+            }
+
+            @Override
+            public void sessionDestroyed(HttpSessionEvent hse) {
+                sessions.remove(hse.getSession().getId());
+            }
+        });
+        return listenerRegistrationBean;
+    }
+}
