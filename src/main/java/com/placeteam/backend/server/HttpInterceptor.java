f6adffc45b9ddf588267bf83eab743da4234fcc2
Lukas Wurzinger <lukas@wrz.one>
2023-03-09 12:17:42 +0100

    Move Session handling

--- src/main/java/com/placeteam/backend/server/HttpInterceptor.java
+++ src/main/java/com/placeteam/backend/server/HttpInterceptor.java
@@ -2,12 +2,35 @@ package com.placeteam.backend.server;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.util.Enumeration;
+
 import org.springframework.web.servlet.HandlerInterceptor;
 
 public class HttpInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        System.err.println("preHandle");
+        HttpSession session = request.getSession();
+
+		Enumeration<String> headerKeys = request.getHeaderNames();
+		while (headerKeys.hasMoreElements()) {
+			String key = headerKeys.nextElement();
+			System.err.println(key + ": " + request.getHeader(key));
+		}
+
+		Object timeStampAttribute = session.getAttribute("timeStamp");
+		Object freshAttribute = session.getAttribute("fresh");
+		if (timeStampAttribute == null || freshAttribute == null) {
+			session.setAttribute("timeStamp", System.currentTimeMillis() / 1000L);
+			session.setAttribute("fresh", true);
+		} else {
+			session.setAttribute("fresh", false);
+		}
+		System.err.printf("Session ID: %s\n", session.getId());
+		System.err.printf("timeStamp: %s\n", session.getAttribute("timeStamp"));
+		System.err.printf("fresh: %b\n", session.getAttribute("fresh"));
+
         return true;
     }
 }
